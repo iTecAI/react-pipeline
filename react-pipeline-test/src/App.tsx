0ab705a434468bf826c94e1be45489cdbad7ce95
Dax Harris <matteovh@gmail.com>
2023-02-23 21:11:57 -0500

    add test app

---
+++ react-pipeline-test/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Pipeline } from "react-pipeline";
+
+function App() {
+    return (
+        <div className="App">
+            <Pipeline />
+        </div>
+    );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-export default function Pipeline() {
+export function Pipeline() {
     return <></>;
 }
